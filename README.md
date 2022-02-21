1818c87061e9bfc2c9df3d4a90349ca9b6263736
KeithJC2000 <jiangchen001012@gmail.com>
2022-02-21 18:29:33 -0500

    229Assignment2
    229Assignment2

---
+++ README.md
@@ -0,0 +1,6 @@
+# COMP229-ASSIGNMENT2
+ ASSIGNMENT2
+ author: Chen Jiang
+ Student#: 301082999
+ Program: COMP229-002
+ Date: 2022/02/19
---
+++ server/config/db.js
@@ -0,0 +1,6 @@
+module.exports = 
+{
+    // "URI": "mongodb://localhost/contact"
+    "URI": "mongodb+srv://Keith:Jc1357911@clusters0002.i0fpg.mongodb.net/COMP229002",
+    "Secret":'SomeSecret'
+}
